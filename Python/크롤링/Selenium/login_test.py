716eff61bbb08ee1f2271e339c9ec4a664c1b5ff
minchan5224 <54893499+minchan5224@users.noreply.github.com>
2021-01-06 16:56:17 +0900

    알리익스프레스 로그인

--- Python/크롤링/Selenium/login_test.py
+++ Python/크롤링/Selenium/login_test.py
@@ -4,30 +4,23 @@ import time
 from selenium.webdriver.common.keys import Keys
 
 start_url = 'https://login.aliexpress.com/buyer.htm?spm=2114.best.1000002.4.2fe5NYviNYviV9&return=https%3A%2F%2Fbest.aliexpress.com%2F%3Flan%3Den%26spm%3Da2g0s.buyerloginandregister.0.0.509c55a3Yemxat&from=lighthouse&random=119478436E545349111FABAB4D2473DA'
+# 위의 url로 접속시 로그인 버튼 클릭할 필요가 없어진다.
 screenshot_name = './screenshot/test_'
 u_id = '' # 아이디
-u_pw = '' # 비번
+u_pw = '' # 비번 
 
+def login_test():
+  '''
+  알리익스프레스 로그인 예시
+  ID 필드와 PW 필드에 값을 넣은후 Keys.RETURN을 통해 로그인
+  '''
   
-def item_search(sel_driver, item_codes):
-    tmp = 0
-    for i in item_codes:
-        search_box = sel_driver.find_element_by_css_selector("input#search-key")
-        search_box.send_keys(i)
-        tmp += 1
-        search_box.send_keys(Keys.RETURN)
-        time.sleep(3)
-        print(sel_driver.current_url)#url 가져오기
-        
-
-
-def main():
     options = Options()
     options.add_argument("--headless")
     options.add_argument("--no-sandbox")
     options.add_argument('--start-fullscreen')
     
-    lode_i_code = ['4000519228140'] #test
+    
 
     driver = webdriver.Chrome(options=options)
     
@@ -41,12 +34,8 @@ def main():
 
     time.sleep(2)
     pw_box.send_keys(Keys.RETURN)
-    time.sleep(2)
-    
-    item_search(driver, lode_i_code)
-    
+    time.sleep(2)   
     driver.quit()
 
-main()
-# test_tt()
-#ui-slidebox-wrap
+login_test()
+
